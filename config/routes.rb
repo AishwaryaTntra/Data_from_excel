4f9d7893aea96b6c3657b789f8db69d481707b22
AishwaryaTntra <aishwarya.pakeyara@tntra.io>
2022-09-19 12:53:48 +0530

    feature/data_importing: modified files

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   # # Defines the root path route ("/")
   root 'cities#index'
-  # post 'locations/new', to: 'locations#new', as: 'new_location'
   resources :data_imports, only: %i[new create]
 
   resources :cities do
@@ -15,16 +14,4 @@ Rails.application.routes.draw do
     resources :users
     resources :messages
   end
-  # resources :locations do
-    
-  # end
-
-  # resources :cities do |city|
-  #   city.resources :locations, name_prefix: 'city_'
-  # end
-
-  # resources :locations do |locations|
-  #   locations.resources :users, name_prefix: 'location_'
-  #   locations.resources :messages, name_prefix: 'location_'
-  # end
 end
