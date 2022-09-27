dda068166a2db26978c091b672aee967267b37d3
AishwaryaTntra <aishwarya.pakeyara@tntra.io>
2022-09-27 15:04:09 +0530

    feature/data_importing: customers controller files

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,28 @@
+class CustomersController < ApplicationController
+  def index
+    location = Location.find_by(id: params[:location_id])
+    @customers = Customer.all.where(location_id: location.id, user_id: current_user.id)
+  end
+
+  def edit
+    @location = Location.find_by(id: params[:location_id])
+    @customer = Customer.find_by(id: params[:id])
+  end
+
+  def update
+    @location = Location.find_by(id: params[:location_id])
+    @customer = Customer.find_by(id: params[:id])
+
+    if @customer.update(user_params)
+      redirect_to location_customers_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :phone, :email, :location_id, :user_id)
+  end
+end
