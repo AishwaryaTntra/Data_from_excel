cc2ed64a7518ca9815091f4cc1a4f2fa938c5028
AishwaryaTntra <aishwarya.pakeyara@tntra.io>
2022-09-27 14:56:11 +0530

    feature/data_importing: modified migrations

--- db/migrate/20220915063653_create_locations.rb
+++ db/migrate/20220915063653_create_locations.rb
@@ -3,6 +3,7 @@ class CreateLocations < ActiveRecord::Migration[7.0]
     create_table :locations do |t|
       t.string :name
       t.references :city, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20220915063806_create_users.rb
+++ db/migrate/20220915063806_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :name
       t.string :email
       t.string :phone
-      t.references :location, null: false, foreign_key: true
 
       t.timestamps
     end
