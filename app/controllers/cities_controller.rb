f17f2500eb3ab1975a76d2e67fb9b817f64bb1d7
AishwaryaTntra <aishwarya.pakeyara@tntra.io>
2022-09-29 11:37:06 +0530

    feature/authorization: Added current_user, method to redirect_to root path if no current user

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,14 +1,16 @@
 class CitiesController < ApplicationController
+  load_and_authorize_resource
+  before_action :authorize
   def index
-    @cities = City.all
+    @cities = City.all.where(user_id: current_user.id)
   end
 
   def new
-    @city = City.new
+    @city = current_user.cities.new
   end
 
   def create
-    @city = City.new(city_params)
+    @city = current_user.cities.new(city_params)
 
     if @city.save
       redirect_to cities_path
@@ -18,11 +20,11 @@ class CitiesController < ApplicationController
   end
 
   def edit
-    @city = City.find_by(id: params[:id])
+    @city = City.find_by(id: params[:id], user_id: current_user.id)
   end
 
   def update
-    @city = City.find_by(id: params[:id])
+    @city = City.find_by(id: params[:id], user_id: current_user.id)
 
     if @city.update(city_params)
       redirect_to cities_path
@@ -32,7 +34,7 @@ class CitiesController < ApplicationController
   end
 
   def destroy
-    @city = City.find_by(id: params[:id])
+    @city = City.find_by(id: params[:id], user_id: current_user.id)
     @city.destroy
 
     redirect_to cities_path, status: :see_other
@@ -41,6 +43,10 @@ class CitiesController < ApplicationController
   private
 
   def city_params
-    params.require(:city).permit(:name)
+    params.require(:city).permit(:name, :user_id)
+  end
+
+  def authorize
+    redirect_to '/' unless current_user
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,10 @@
 class MessagesController < ApplicationController
+  load_and_authorize_resource
+  before_action :authorize
   def index
     @location = Location.find_by(id: params[:location_id])
     @messages = Message.all.where(location_id: @location.id)
-    unless @messages.present?
-      redirect_to new_location_message_path(@location), notice: 'No messages sent yet!'
-    end
+    redirect_to new_location_message_path(@location), notice: 'No messages sent yet!' unless @messages.present?
   end
 
   def new
@@ -12,7 +12,7 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
-  def create 
+  def create
     @location = Location.find_by(id: params[:location_id])
     @message = @location.messages.new(message_params)
     if @message.save
@@ -27,5 +27,9 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:body, :title)
+  end
+
+  def authorize
+    redirect_to '/' unless current_user
   end
 end
