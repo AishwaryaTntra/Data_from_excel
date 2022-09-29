6e833f0dea89e837e93281b8e31835a34e5b18da
AishwaryaTntra <aishwarya.pakeyara@tntra.io>
2022-09-29 11:57:11 +0530

    feature/authorization: devise set_up, cancan setup, default role method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,25 @@
 # require activerecord - import
 class User < ActiveRecord::Base
-  has_many :cities
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :cities, dependent: :destroy
   has_many :locations
   has_many :customers
+  belongs_to :role
   validates :name, :phone, :email, presence: true
+  after_initialize :set_default_role
+
+  %w[admin user].each do |role|
+    define_method "#{role}?" do
+      self.role == Role.find_by(name: role.titleize)
+    end
+  end
+
+  private
+
+  def set_default_role
+    self.role ||= Role.find_by(name: 'User')
+  end
 end
